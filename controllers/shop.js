145c8f02b465a3bd22a1cff6194dcddbc31747aa
srms28 <70207074+srms28@users.noreply.github.com>
2021-03-06 10:14:42 +0530

    Update shop.js

--- controllers/shop.js
+++ controllers/shop.js
@@ -120,30 +120,30 @@ exports.postReview=(req,res,next)=>{
 }
 
 
-exports.getProduct = (req, res, next) => {
-  const prodId = req.params.productId;
-  Product.findById(prodId)
-    .then(product=>{
-      var spawn = require('child_process').spawn;
-      console.log('hello console');
-      var process = spawn('python', ['./rec.py',product.title]);
-      process.stdout.on('data', function(data) { 
-        console.log(data.toString());
-        // res.send(data.toString()); 
-    } ) 
-    })
-    .catch(err => console.log(err));
-  Product.findById(prodId)
-    .then(product => {
-      console.log('hello console1');
-      res.render('shop/product-detail', {
-        product: product,
-        pageTitle: product.title,
-        path: '/products'
-      });
-    })
-    .catch(err => console.log(err));
-};
+// exports.getProduct = (req, res, next) => {
+//   const prodId = req.params.productId;
+//   Product.findById(prodId)
+//     .then(product=>{
+//       var spawn = require('child_process').spawn;
+//       console.log('hello console');
+//       var process = spawn('python', ['./rec.py',product.title]);
+//       process.stdout.on('data', function(data) { 
+//         console.log(data.toString());
+//         // res.send(data.toString()); 
+//     } ) 
+//     })
+//     .catch(err => console.log(err));
+//   Product.findById(prodId)
+//     .then(product => {
+//       console.log('hello console1');
+//       res.render('shop/product-detail', {
+//         product: product,
+//         pageTitle: product.title,
+//         path: '/products'
+//       });
+//     })
+//     .catch(err => console.log(err));
+// };
 
 
 // Product.findById(prodId)
